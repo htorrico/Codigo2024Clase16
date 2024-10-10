51f64ebc1e7b0bb8a7848876a3bc31e2dc004363
Hugo Torrico <torricomarquezh@gmail.com>
2024-10-09 21:35:32 -0500

    FacturadorDIP

---
+++ DIP/FacturadorDIP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    public class FacturadorDIP
+    {
+        public void RealizarPedido(INotificador notificador)
+        {
+            Console.WriteLine("Se acaba de realizar el pedido");
+            notificador.Notificar();
+        }
+    }
+}
---
+++ DIP/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+using DIP;
+
+Facturador facturador = new Facturador();
+
+facturador.RealizarPedido(1);
+
+facturador.RealizarPedido(2);
+
+facturador.RealizarPedido(3);
+
+Console.WriteLine("Usando Principios SOLID");
+
+FacturadorDIP facturadorDIP = new FacturadorDIP();
+
+facturadorDIP.RealizarPedido(new Email());
+
+facturadorDIP.RealizarPedido(new Push());
+
+facturadorDIP.RealizarPedido(new SMS());
+
+Console.Read();
